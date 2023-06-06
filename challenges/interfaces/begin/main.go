ee2d59f403aebd98bc4b032aeff91a5a5f705781
Daniel Cruz <daniel.cruz.civieta@gmail.com>
2023-06-06 09:14:03 +0000

    do interfaces challenge

--- challenges/interfaces/begin/main.go
+++ challenges/interfaces/begin/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"unicode"
 
 	"github.com/davecgh/go-spew/spew"
 )
@@ -16,9 +17,51 @@ type counter interface {
 
 type letterCounter struct{ identifier string }
 
+func (letterCounter letterCounter) name() string {
+	return letterCounter.identifier
+}
+
+func (letterCounter letterCounter) count(input string) int {
+	var counter int = 0
+	for _, letter := range input {
+		if unicode.IsLetter(letter) {
+			counter++
+		}
+	}
+	return counter
+}
+
 type numberCounter struct{ designation string }
 
+func (numberCounter numberCounter) name() string {
+	return numberCounter.designation
+}
+
+func (numberCounter numberCounter) count(input string) int {
+	var counter int = 0
+	for _, letter := range input {
+		if unicode.IsDigit(letter) {
+			counter++
+		}
+	}
+	return counter
+}
+
 type symbolCounter struct{ label string }
+
+func (symbolCounter symbolCounter) name() string {
+	return symbolCounter.label
+}
+
+func (symbolCounter symbolCounter) count(input string) int {
+	var counter int = 0
+	for _, letter := range input {
+		if !unicode.IsDigit(letter) && !unicode.IsLetter(letter) {
+			counter++
+		}
+	}
+	return counter
+}
 
 func doAnalysis(data string, counters ...counter) map[string]int {
 	// initialize a map to store the counts
@@ -55,7 +98,12 @@ func main() {
 	spew.Dump(data)
 
 	// call doAnalysis and pass in the data and the counters
+	letterCounter := letterCounter{identifier: "letterCounter"}
+	numberCounter := numberCounter{designation: "numberCounter"}
+	symbolCounter := symbolCounter{label: "symbolCounter"}
+
+	analysis := doAnalysis(data, letterCounter, numberCounter, symbolCounter)
 
 	// dump the map to the console using the spew package
-	// spew.Dump(analysis)
+	spew.Dump(analysis)
 }
