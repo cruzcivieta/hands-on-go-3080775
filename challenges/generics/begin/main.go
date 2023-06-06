3ca05862cb71c36ec7a4afecd6dcdc2cf26592ad
Daniel Cruz <daniel.cruz.civieta@gmail.com>
2023-06-06 10:09:50 +0000

    do generic challenge

--- challenges/generics/begin/main.go
+++ challenges/generics/begin/main.go
@@ -4,6 +4,9 @@ package main
 import "fmt"
 
 // Part 1: print function refactoring
+func printAny[T string | int | bool](value T) {
+	fmt.Println(value)
+}
 
 // non-generic print functions
 
@@ -14,6 +17,17 @@ func printInt(i int) { fmt.Println(i) }
 func printBool(b bool) { fmt.Println(b) }
 
 // Part 2 sum function refactoring
+type numeric interface {
+	~int | ~float64
+}
+
+func sumAny[T numeric](values ...T) T {
+	var total T
+	for _, value := range values {
+		total += value
+	}
+	return total
+}
 
 // sum sums a slice of any type
 func sum(numbers []interface{}) interface{} {
@@ -33,14 +47,19 @@ func sum(numbers []interface{}) interface{} {
 
 func main() {
 	// call non-generic print functions
-	printString("Hello")
-	printInt(42)
-	printBool(true)
+	// printString("Hello")
+	// printInt(42)
+	// printBool(true)
 
 	// call generic printAny function for each value above
+	printAny("Hello")
+	printAny(42)
+	printAny(true)
 
 	// call sum function
 	fmt.Println("result", sum([]interface{}{1, 2, 3}))
 
 	// call generics sumAny function
+	fmt.Println("result", sumAny(2.5, 2.7, 3.9))
+	fmt.Println("result", sumAny(2, 3, 5))
 }
